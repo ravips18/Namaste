68cb672aba0b5c667b7efe9b20a68f066c535eb5
Ravi PS <info@aaruni.co>
2025-03-26 11:03:54 +0530

    Initial commit

---
+++ src/com/example/namaste/namaste.java
@@ -0,0 +1,10 @@
+package com.example.namaste;
+
+public class namaste {
+	public static void main(String  args[])
+	{
+		System.out.print("Namaste All! \n");
+		
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author RaviPS
+ *
+ */
+module Namaste {
+}
