b6b52b986ecdde4e833136959d319a0900ed801e
Ravi PS <info@aaruni.co>
2025-03-29 19:03:56 +0530

    2nd Commit

--- src/com/example/namaste/namaste.java
+++ src/com/example/namaste/namaste.java
@@ -1,10 +1,19 @@
 package com.example.namaste;
 
+
+
 public class namaste {
-	public static void main(String  args[])
+
+	public static void main(String args[]) 
 	{
-		System.out.print("Namaste to  All! \n");
-		System.out.print("Namaste to  Guests!  \n"); 
+		
+		try {
+
+			System.out.print("Hello Leelavati! ");
+
+		} catch (Exception ex) {
+			
+		}
 	}
 
 }
